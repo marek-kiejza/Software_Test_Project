4f5e60f3093a57ddcdc6a31bf94827aa5d9df480
Marek Kiejza <Marek.Kiejza@mail.itsligo.ie>
2019-03-14 11:33:31 +0000

    com

---
+++ Mock1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mock1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+      
+    }
+    
+
+    public class CustomerService
+    {
+
+        public virtual int GetValue()
+        {
+            return 11;
+        }
+    }
+
+
+    public class Customer
+    {
+        private readonly CustomerService _cs;
+
+        public Customer(CustomerService cs)
+        {
+            _cs = cs;
+        }
+
+        public int CalcDiscount()
+        {
+            return _cs.GetValue() * 2;
+        }
+
+    }
+
+}
---
+++ Mock1/TestMock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Telerik.JustMock;
+
+
+namespace Mock1
+{
+    [TestFixture]
+    class TestMock
+    {
+        [Test]
+        public void MyTestMethod()
+        {
+            //Arrange
+            //var cs = new Mock<CustomerService>();
+
+            //act
+           // cs.S
+        }
+    }
+}
